a578b75f317fc5ea65d297eabe4231a217896311
XUZMA <xuzmnsccsz@yeah.net>
2015-06-18 14:39:45 +0800

    lym p.36

---
+++ liyamin/mux2x1_andor_test.v
@@ -0,0 +1,29 @@
+`include "mux2x1_andor.v"
+
+module mux2x1_3s_test;
+   reg s, a0, a1;
+   wire y;
+
+   mux2x1_andor mux2x1(y, s, a0, a1);
+
+   initial
+     begin
+	s = 0; a0 = 0; a1 = 0;
+	#1 s = 1; a0 = 0; a1 = 0;
+	#1 s = 0; a0 = 1; a1 = 0;
+	#1 s = 1; a0 = 1; a1 = 0;
+	#1 s = 0; a0 = 0; a1 = 1;
+	#1 s = 1; a0 = 0; a1 = 1;
+	#1 s = 0; a0 = 1; a1 = 1;
+	#1 s = 1; a0 = 1; a1 = 1;
+	#1 $finish;
+     end
+
+   initial
+     begin
+	$monitor($time, ":\ts=%b", s, "\ta0=%b", a0, "\ta1=%b", a1, "\ty=%b", y);
+	$dumpfile("mux2x1_andor_test.vcd");
+	$dumpvars;
+     end
+   
+endmodule // mux2x1_3s_test
